e4ec313ccbbdfdcfa11b2ea94799ec9c50e2d164
David Nunes <ddnr03@gmail.com>
2025-08-18 17:50:16 -0300

    errorHandler

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
@@ -9,6 +9,13 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'Servidor rodando!' });
 });
 
+// Rota para forçar erro de teste
+app.get('/erro', (req, res, next) => {
+  const err = new Error("Algo deu errado de propósito!");
+  err.status = 500; 
+  next(err); // passa o erro para o errorHandler
+});
+
 // Importação das rotas
 const agentesRouter = require('./routes/agentesRoutes');
 const casosRouter = require('./routes/casosRoutes');
@@ -17,6 +24,12 @@ const casosRouter = require('./routes/casosRoutes');
 app.use('/agentes', agentesRouter);
 app.use('/casos', casosRouter);
 
+// Importação do middleware de erro
+const errorHandler = require('./utils/errorHandler');
+
+// Registro do middleware de erro
+app.use(errorHandler);
+
 app.listen(PORT, () => {
-    console.log(`Servidor do Departamento de Polícia rodando em localhost:${PORT}`);
+  console.log(`Servidor do Departamento de Polícia rodando em localhost:${PORT}`);
 });
